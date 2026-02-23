2a34a2c7e4fa8047f358d06e2a56f7060c14eede
Syntrax <dannycloudstorage1@gmail.com>
2026-02-23 12:20:17 +0100

    Update Syntrax NovaBook.py

--- Syntrax NovaBook.py
+++ Syntrax NovaBook.py
@@ -1,3 +1,20 @@
+#Copyright (C) 2026 Syntrax Technologies
+
+#This file is part of Syntrax Nova.
+
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU Affero General Public License as published by the
+#Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#See the GNU Affero General Public License for more details.
+
+#You should have received a copy of the GNU Affero General Public License
+#along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 import tkinter as tk
 from tkinter import ttk, filedialog, colorchooser, simpledialog
 import json
@@ -272,4 +289,5 @@ class NovaBook:
 
 root = tk.Tk()
 app = NovaBook(root)
+
 root.mainloop()
